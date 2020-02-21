652b4e641c602ff6d137540816d943fab6277d44
006373 <Taras.Nedohybchenko@bank.gov.ua>
2020-02-21 16:40:53 +0200

    Add Binary Search Tree (BST)

--- BinaryTreeSearch.py
+++ BinaryTreeSearch.py
@@ -1,35 +1,67 @@
-class TreeItem:
+class BinaryTreeItem:
 
-    def __init__(self, value=0):
+    def __init__(self, value=0, name=''):
         self.leftItem=None
         self.rightItem=None
         self.value=value
+        self.name=name
+
+    def __str__(self):
+        return("name: %s, leftItem : %s, rightItem: %s, value: %s"%(self.name, self.leftItem,self.rightItem,self.value))
 
 
-ti0=TreeItem(55)
-ti1=TreeItem(50)
-ti2=TreeItem(20)
-ti3=TreeItem(62)
-ti4=TreeItem(21)
-ti5=TreeItem(74)
-ti6=TreeItem(81)
-ti7=TreeItem(7)
-ti8=TreeItem(1)
-ti9=TreeItem(95)
 
 
-ti0.leftItem=ti1    # 70>50
-ti0.rightItem=ti3   # 70<62
+def bts(item,value):
 
-ti1.leftItem=ti2    # 50>20
-ti1.rightItem=ti5   # 50<74
+    if item==None:
+        return None
 
-ti3.leftItem=ti4    # 62>21
+    if item.value==value:
+        return item
+
+    if item.value>value:
+        return bts(item.leftItem,value)
+    else:
+        return bts(item.rightItem,value)
+
+
+
+ti0=TreeItem(55,'ti0')
+ti1=TreeItem(30,'ti1')
+ti2=TreeItem(20,'ti2')
+ti3=TreeItem(62,'ti3')
+ti4=TreeItem(57,'ti4')
+ti5=TreeItem(50,'ti5')
+ti6=TreeItem(81,'ti6')
+ti7=TreeItem(7,'ti7')
+ti8=TreeItem(1,'ti8')
+ti9=TreeItem(25,'ti9')
+
+
+ti0.leftItem=ti1    # 55>30
+ti0.rightItem=ti3   # 55<62
+
+ti1.leftItem=ti2    # 30>20
+ti1.rightItem=ti5   # 30<50
+
+ti3.leftItem=ti4    # 62>57
 ti3.rightItem=ti6   # 62<81
 
 ti2.leftItem=ti7    # 20>7
-ti2.rightItem=ti9   # 20<95
+ti2.rightItem=ti9   # 20<25
 
-ti3.leftItem=ti9    # 62>1
+ti7.leftItem=ti8    # 7>1
 
 
+itm=bts(ti0,5)
+print(itm)
+
+itm=bts(ti0,120)
+print(itm)
+
+itm=bts(ti0,7)
+print(itm)
+
+itm=bts(ti0,81)
+print(itm)
